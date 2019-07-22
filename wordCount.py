f7d16e708fe15eb1cbfe7c1871b01b76e3b8db8f
whereiszack <zobertson@gmail.com>
2019-07-22 12:39:01 -0700

    first draft (not working)

---
+++ wordCount.py
@@ -0,0 +1,16 @@
+#this program calculates the number of words inputted by a user
+
+def main():
+    print("This program calculates the number of words in a sentencence")
+
+    #get the sentenece from the user
+    wordCount = eval(input("Please write a sentence you'd like the word count for: "))
+    WordCount.count(" " [-1])
+
+    print("Your word count is: ")
+
+main()
+
+
+
+        
